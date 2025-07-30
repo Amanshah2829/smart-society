9869d6afa4d358a86373b6d20540eb2a42b61ab6
bl4ck9998 <bl4ck9998@gmail.com>
2025-07-31 05:26:28 +0530

    commit

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,7 +1,7 @@
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/smart-society';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://bl4ck9998:sgfoNUgL6KwyDct3@cluster0.uudljof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
